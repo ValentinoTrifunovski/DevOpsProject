87851c941bc59b6208840e70717d33ca573e388b
Valentino Trifunovski <valentino.trifunovski@gmail.com>
2023-09-12 15:58:51 +0200

    Updates

---
+++ precache-manifest.dacf621d5eabf2375e94e7970eb1f4b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0448c2dfb3216bf9830311b279673c",
+    "url": "/DevOpsProject/index.html"
+  },
+  {
+    "revision": "4d17206f1c4c53fb7197",
+    "url": "/DevOpsProject/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "616cf738a6296bd2444f",
+    "url": "/DevOpsProject/static/css/main.b123325e.chunk.css"
+  },
+  {
+    "revision": "4d17206f1c4c53fb7197",
+    "url": "/DevOpsProject/static/js/2.3daea747.chunk.js"
+  },
+  {
+    "revision": "cc2c2b9c0967890c7a3a4f09b5212b11",
+    "url": "/DevOpsProject/static/js/2.3daea747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616cf738a6296bd2444f",
+    "url": "/DevOpsProject/static/js/main.452dd636.chunk.js"
+  },
+  {
+    "revision": "25f3cc767b4ca8312781",
+    "url": "/DevOpsProject/static/js/runtime-main.9eb9f983.js"
+  },
+  {
+    "revision": "82151fa82a8c5cf6b637445e311cd4a4",
+    "url": "/DevOpsProject/static/media/KeyStoreLogo.82151fa8.png"
+  },
+  {
+    "revision": "8273c5b09e49f396a9394c47f3fb9205",
+    "url": "/DevOpsProject/static/media/car_keychains_image.8273c5b0.jpg"
+  },
+  {
+    "revision": "9dc617b86bf0115b805e418d01d985b5",
+    "url": "/DevOpsProject/static/media/car_keys_image.9dc617b8.jfif"
+  },
+  {
+    "revision": "2c8ddfdd69968081ce07cb97826b1417",
+    "url": "/DevOpsProject/static/media/finki_icon.2c8ddfdd.png"
+  }
+]);
